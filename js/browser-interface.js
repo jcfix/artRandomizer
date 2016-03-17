efadc4a9874e3b96909a489272a092d54a337537
Jessica Fix and Taylor Pokoj <pokojt@gmail.com>
2016-03-17 10:40:43 -0700

    Make nasa photo the background image and append art in space

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -2,14 +2,35 @@
 // var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
+
+  $.get("https://api.nasa.gov/planetary/apod?api_key=FZqzzfC0EjAiSrALPbuOtaKiiVZfQDMqmDcyB85z").then(function(response) {
+
+    var image = response.url;
+
+
+    $(".result").append('<div class="spaceContent" style="background-image: url('+image+')"></div><div class="spaceText"><h3>NASA Space Photo of the Day </h3><p>'+response.explanation+'</p></div>');
+    console.log(response);
+  });
+
   $("button#clearResult").hide();
+  $("button#addToCollection").hide();
   $("form.bookSearch").submit(function(event) {
 
     $('.header').addClass('searchHeader');
     $("button#clearResult").show();
+    $("button#addToCollection").show();
     event.preventDefault();
 
     var inputtedSearch = $("#search").val();
+
+    // $.get("https://api.nasa.gov/planetary/apod?api_key=FZqzzfC0EjAiSrALPbuOtaKiiVZfQDMqmDcyB85z").then(function(response) {
+    //
+    //   var image = response.url;
+    //
+    //
+    //   $(".result").append('<div class="spaceContent" style="background-image: url('+image+')"></div><div class="spaceText"><p>'+response.explanation+'</p></div>');
+    //   console.log(response);
+    // });
 
     $.get('http://www.europeana.eu/api/v2/search.json?wskey=EXCJeCLoy&query='+ inputtedSearch +'').then(function(response) {
       console.log(response);
@@ -17,28 +38,25 @@ $(document).ready(function() {
       var title = response.items[0].title;
       var thumbnail = response.items[0].edmPreview;
 
-      $(".result").append('<img class="artImage "src="'+thumbnail+'" alt="'+title+'">');
-      $(".result").append("Top Search Results: " + title + "!");
+      $(".artContent").append('<div class="artItem"><img class="artImage "src="'+thumbnail+'" alt="'+title+'"><p class="artText">Top Search Results: ' + title + '!</p></div>');
 
     });
 
 
-    $.get("https://api.nasa.gov/planetary/apod?api_key=FZqzzfC0EjAiSrALPbuOtaKiiVZfQDMqmDcyB85z").then(function(response) {
-
-      var image = response.url;
-
-      $(".result").append('<img class="spaceImage" src="'+image+'" alt="">');
-      console.log(response);
-    });
     $("form.bookSearch")[0].reset();
 
     $("button#clearResult").click(function() {
-    $("div.result").empty();
+    $("div.artContent").empty();
     $("button#clearResult").hide();
     $('.header').removeClass('searchHeader');
+    $("button#addToCollection").hide();
     });
   });
 });
+
+
+// $(".result").append('<div class="spaceContent"><img class="spaceImage" src="'+image+'" alt=""><br><p class="spaceText">'+response.explanation+'</p></div>');
+// console.log(response);
 
 
 // /* Front-end User interface (jquery) code goes in this file */
